b536f1dcab4053c4f570ba9f7dc8bf6cc9dee998
YDKD <1606354057@qq.com>
2022-10-26 22:31:52 +0800

    feat(router): 添加navigate和404

--- src/27_router/01_初识router/App.tsx
+++ src/27_router/01_初识router/App.tsx
@@ -1,7 +1,11 @@
 import React, { PureComponent } from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Navigate, NavLink, Route, Routes } from 'react-router-dom'
+
+import './style.less'
+
 import About from './views/About'
 import Home from './views/Home'
+import NotFound from './views/NotFound'
 
 export class App extends PureComponent {
 	render() {
@@ -9,12 +13,26 @@ export class App extends PureComponent {
 			<div>
 				<h1>Header</h1>
 				<div className="header">
-					<Link to="/home">home</Link>
-					<Link to="/about">About</Link>
+					{/* <Link to="/home">home</Link>
+					<Link to="/about">About</Link> */}
+					<NavLink
+						to="/home"
+						style={() => ({
+							color: 'blue'
+						})}
+					>
+						home
+					</NavLink>
+					<NavLink to="/about">About</NavLink>
 				</div>
 				<Routes>
+					{/* navigate可以处理当我们访问一个路径时，直接重定向到另外一个路由 */}
+					<Route path="/" element={<Navigate to="/home" />}></Route>
+
 					<Route path="/home" element={<Home />}></Route>
 					<Route path="/about" element={<About />}></Route>
+					<Route path="/404" element={<NotFound />}></Route>
+					<Route path="*" element={<Navigate to="/404" />}></Route>
 				</Routes>
 				<h1>Footer</h1>
 			</div>
---
+++ src/27_router/01_初识router/views/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class index extends PureComponent {
+  render() {
+    return (
+      <div>
+        这里是404页面
+      </div>
+    )
+  }
+}
+
+export default index
